c6d383dcf804c0e9df2dcbddd34acc201c3c6b2e
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-01 19:34:21 +0200

    Initial prototype

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <div id="app">
-    
+    <input type="text" @input="input" />
+    <h1>{{ title }}</h1>
+    <button @click="changeTitle">Press Me</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+      title: "Hello World!!!",
+    }
+  },
+  methods: {
+    input(event) {
+      this.message = event.target.value;
+      console.log(this.message);
+    },
+    changeTitle() {
+      this.title = this.message;
+    }
+  }
+}
+</script>
